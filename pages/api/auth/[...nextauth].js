db14bdfbac5cae9a371f63531ffd5b714b4ca038
Edward <edward_t@mbp-edward.home>
2023-05-03 10:18:39 +0200

    commit

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -11,6 +11,9 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  authorization: {
+    params: { prompt: "login" }
+  }
 }
 
 export default NextAuth(authOptions) 
